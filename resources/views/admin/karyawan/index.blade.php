6529083575af2d06e2df8e9d62a9cf98bd9ec1e1
Nurbismii <nurbismi74@gmail.com>
2026-02-25 16:16:08 +0800

    test: add select all kategori resign

--- resources/views/admin/karyawan/index.blade.php
+++ resources/views/admin/karyawan/index.blade.php
@@ -85,6 +85,7 @@
 
                             <div class="col-md-3">
                                 <select id="filter_resign" class="form-select">
+                                    <option value="">Semua Kategori Resign</option>
                                     <option value="AKTIF" selected>Aktif</option>
                                     <option value="RESIGN SESUAI PROSEDUR">Resign Sesuai Prosedur</option>
                                     <option value="RESIGN TIDAK SESUAI PROSEDUR">Resign Tidak Sesuai Prosedur</option>
